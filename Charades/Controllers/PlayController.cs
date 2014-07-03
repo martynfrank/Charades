6bd53df95a7c6674fd768a12945048fd2f4fc5de
MartynFrank <martyn.frank.87@gmail.com>
2014-07-03 21:08:32 +0100

    Add Knockout and change controller to return Json

--- Charades/Controllers/PlayController.cs
+++ Charades/Controllers/PlayController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using Charades.Business.Interface;
 
@@ -18,7 +19,12 @@ namespace Charades.Controllers
 
         public ActionResult Index()
         {
-            return View(_phraseModule.GetRandomPhrase());
+            return View();
+        }
+
+        public JsonResult GetRandomPhrase()
+        {
+            return Json ( _phraseModule.GetRandomPhrase() );
         }
     }
 }
